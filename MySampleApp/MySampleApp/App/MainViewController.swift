95697be0b3417aac562803a77894a3eb8b3c1a8e
Justin Lee <Justin@jmlee-mbp.student.iastate.edu>
2017-02-25 08:33:33 -0600

    New branch to experiment with tabView Changes

--- MySampleApp/MySampleApp/App/MainViewController.swift
+++ MySampleApp/MySampleApp/App/MainViewController.swift
@@ -14,9 +14,8 @@
 import UIKit
 import AWSMobileHubHelper
 
-class MainViewController: UITableViewController {
+class MainViewController: UITabBarController {
     
-    var demoFeatures: [DemoFeature] = []
     var signInObserver: AnyObject!
     var signOutObserver: AnyObject!
     fileprivate let loginButton: UIBarButtonItem = UIBarButtonItem(title: nil, style: .done, target: nil, action: nil)
@@ -24,6 +23,7 @@ class MainViewController: UITableViewController {
     // MARK: - View lifecycle
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
         
@@ -34,24 +34,8 @@ class MainViewController: UITableViewController {
 
             presentSignInViewController()
         
-        var demoFeature = DemoFeature.init(
-            name: NSLocalizedString("User Sign-in",
-                comment: "Label for demo menu option."),
-            detail: NSLocalizedString("Enable user login with popular 3rd party providers.",
-                comment: "Description for demo menu option."),
-            icon: "UserIdentityIcon", storyboard: "UserIdentity")
         
-        demoFeatures.append(demoFeature)
         
-        demoFeature = DemoFeature.init(
-            name: NSLocalizedString("NoSQL",
-                comment: "Label for demo menu option."),
-            detail: NSLocalizedString("Store data in the cloud.",
-                comment: "Description for demo menu option."),
-            icon: "NoSQLIcon", storyboard: "NoSQLDatabase")
-        
-        demoFeatures.append(demoFeature)
-
                 signInObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name.AWSIdentityManagerDidSignIn, object: AWSIdentityManager.default(), queue: OperationQueue.main, using: {[weak self] (note: Notification) -> Void in
                         guard let strongSelf = self else { return }
                         print("Sign In Observer observed sign in.")
@@ -92,27 +76,7 @@ class MainViewController: UITableViewController {
     
     // MARK: - UITableViewController delegates
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MainViewCell")!
-        let demoFeature = demoFeatures[indexPath.row]
-        cell.imageView!.image = UIImage(named: demoFeature.icon)
-        cell.textLabel!.text = demoFeature.displayName
-        cell.detailTextLabel!.text = demoFeature.detailText
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return demoFeatures.count
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let demoFeature = demoFeatures[indexPath.row]
-        let storyboard = UIStoryboard(name: demoFeature.storyboard, bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: demoFeature.storyboard)
-        self.navigationController!.pushViewController(viewController, animated: true)
-    }
-
+  
     
     
     func handleLogout() {
